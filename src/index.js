9bc06ba00826286b41b860b467f90cda76230d5f
nethmih <nethmi.hettiarachchi@trycake.com>
2017-05-21 14:49:47 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import App from './components/App';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-const Index = React.createClass({
-  render () {
-    return (
-      <Router history={browserHistory}>
-        <Route path='/'>
-          <IndexRoute component={App} />
-        </Route>
-      </Router>
-    );
-  }
-});
+const Index = () => {
+  return (
+    <Router history={browserHistory}>
+      <Route path='/'>
+        <IndexRoute component={App} />
+      </Route>
+    </Router>
+  );
+};
 
 export default Index;
